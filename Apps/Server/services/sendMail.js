8a9848775931006f45190acb1370ad8dd7bb264e
TAMIL AZHAGAN <mtamilazhagan6@gmail.com>
2026-07-20 21:38:54 +0530

    revert: switch back to Gmail SMTP

--- Apps/Server/services/sendMail.js
+++ Apps/Server/services/sendMail.js
@@ -1,36 +1,30 @@
-import axios from "axios";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const sendEmail = async (to, subject, html) => {
-  const response = await axios.post(
-    "https://api.brevo.com/v3/smtp/email",
-    {
-      sender: {
-        name: "Verdique Living",
-        email: process.env.EMAIL_USER,
-      },
-      to: [
-        {
-          email: to,
-        },
-      ],
-      subject,
-      htmlContent: html,
-    },
-    {
-      headers: {
-        "api-key": process.env.EMAIL_PASS,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("API KEY PREFIX:", process.env.EMAIL_PASS?.substring(0, 8));
-  console.log("Email sent:", response.data);
+import nodemailer from "nodemailer";
 
-  return response.data;
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = async (to, subject, html) => {
+  const info = await transport.sendMail({
+    from: {
+      name: "Verdique Living",
+      address: process.env.EMAIL_USER,
+    },
+    to,
+    subject,
+    html,
+  });
+
+  console.log("Email sent:", info.response);
+
+  return info;
 };
 
 export default sendEmail;
