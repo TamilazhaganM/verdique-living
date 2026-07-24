35fcb9d1a6b63d27547e804540d80942cd18a04a
TAMIL AZHAGAN <mtamilazhagan6@gmail.com>
2026-07-24 20:54:01 +0530

    fix:nodemailer fixed

--- Apps/Server/server.js
+++ Apps/Server/server.js
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import dns from "dns";
+
 
 
 const PORT = process.env.PORT || 5000;
-
+dns.setDefaultResultOrder("ipv4first");
 
 const startServer = async () => {
     try {
--- Apps/Server/services/sendMail.js
+++ Apps/Server/services/sendMail.js
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import nodemailer from "nodemailer";
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 const transport = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
